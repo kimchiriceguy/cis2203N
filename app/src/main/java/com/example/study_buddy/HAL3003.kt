6046b8d04181f841cb4f1363b5f96e84b53fd82a
kimchiriceguy <ricekimchirice1@gmail.com>
2026-02-17 21:29:17 +0800

    an attempt at the recyclerView challenge thing

--- app/src/main/java/com/example/study_buddy/HAL3003.kt
+++ app/src/main/java/com/example/study_buddy/HAL3003.kt
@@ -8,20 +8,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HAL3003 : AppCompatActivity() {
-    lateinit var titleUsername : TextView
-    lateinit var button3 : Button
+
+    private lateinit var titleUsername: TextView
+    private lateinit var button3: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_hal3003)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val items = listOf(
+            Item("Joe Smoe", "J@gmail.com", R.drawable.a),
+            Item("Doe Smoe", "D@gmail.com", R.drawable.b),
+            Item("Poe Smoe", "P@gmail.com", R.drawable.c),
+            Item("Coe Smoe", "C@gmail.com", R.drawable.d),
+            Item("Zoe Smoe", "Z@gmail.com", R.drawable.e),
+            Item("Loe Smoe", "L@gmail.com", R.drawable.f)
+        )
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(items)
 
         titleUsername = findViewById(R.id.testView)
         button3 = findViewById(R.id.button3)
@@ -30,9 +47,9 @@ class HAL3003 : AppCompatActivity() {
         titleUsername.text = "I'm afraid I can't do that, ${name ?: "userguy"}."
 
         button3.setOnClickListener {
-            val hal3004 = Intent(this, HAL3004::class.java)
-            hal3004.putExtra("username2", name)
-            startActivity(hal3004)
+            val intent = Intent(this, HAL3004::class.java)
+            intent.putExtra("username2", name)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/study_buddy/Item.kt
@@ -0,0 +1,7 @@
+package com.example.study_buddy
+
+data class Item(
+        val name: String,
+        val email: String,
+        val imageResId: Int
+)

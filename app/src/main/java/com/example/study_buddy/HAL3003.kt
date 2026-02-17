2eb2ecfd3e41c7ff418b83ef3c31f937f1d44744
kimchiriceguy <ricekimchirice1@gmail.com>
2026-02-17 19:41:42 +0800

    init commit

---
+++ app/src/main/java/com/example/study_buddy/HAL3003.kt
@@ -0,0 +1,38 @@
+package com.example.study_buddy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HAL3003 : AppCompatActivity() {
+    lateinit var titleUsername: TextView
+    lateinit var button3: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_hal3003)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        titleUsername = findViewById(R.id.testView)
+        button3 = findViewById(R.id.button3)
+
+        val name = intent.getStringExtra("username")
+        titleUsername.text = "Hello. ${name ?: "userguy"}."
+
+        button3.setOnClickListener {
+            val hal3004 = Intent(this, HAL3004::class.java)
+            hal3004.putExtra("username2", name)
+            startActivity(hal3004)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/study_buddy/hal3005.kt
@@ -0,0 +1,38 @@
+package com.example.study_buddy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class hal3005 : AppCompatActivity() {
+    lateinit var displayName : TextView
+    lateinit var button5 : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_hal3005)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        displayName = findViewById(R.id.displayName)
+        button5 = findViewById(R.id.backButton)
+        val usernameTitle = intent.getStringExtra("username2")
+        displayName.text = "Thaks for visiting ${usernameTitle ?: "Userguy"}"
+
+        button5.setOnClickListener {
+            val intentBack = Intent(this, MainActivity::class.java)
+
+            startActivity(intentBack)
+            finish()
+        }
+
+    }
+}

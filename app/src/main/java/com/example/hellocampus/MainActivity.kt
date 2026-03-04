69f446d86067c1cf467a8e9194bf8e59fc0d83a0
kimchiriceguy <ricekimchirice1@gmail.com>
2026-03-04 18:45:49 +0800

    Reposted act 1 | exercise1

---
+++ app/src/main/java/com/example/hellocampus/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.hellocampus
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val myToolbar: Toolbar = findViewById(R.id.myToolbar)
+        setSupportActionBar(myToolbar)
+
+        supportActionBar?.title = "HelloCampus - 19800037"
+
+        val toAngie: Button = findViewById(R.id.toAngie)
+        val ttAngie: TextView = findViewById(R.id.tvdept)
+
+        toAngie.setOnClickListener {
+            ttAngie.text = "Angie Ceniza"
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
